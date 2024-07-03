ac2a20260b2dd8a2d2d03f54241fb13f69ae549c
barni363hun <barni.pbs@gmail.com>
2024-07-03 12:48:42 +0200

    ExchangeRateTable component created

---
+++ src/components/ExchangeRateTable.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>table</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+export default class ExchangeRateTable extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
