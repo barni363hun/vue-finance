b506526e4eb9913ba29dbbbc9f997304bdaa2e6d
barni363hun <barni.pbs@gmail.com>
2024-07-03 15:09:42 +0200

    gave up soapservice

--- src/components/ExchangeRateTable.vue
+++ src/components/ExchangeRateTable.vue
@@ -7,19 +7,10 @@
 <script lang="ts">
 import { Vue } from "vue-class-component";
 import { ref, onMounted } from "vue";
-import { fetchUsers } from "@/services/soapService";
+// import { makeSoapRequest } from "@/services/soapService";
 export default class ExchangeRateTable extends Vue {
   mounted() {
-    this.fetchUsersData();
-  }
-
-  private async fetchUsersData() {
-    try {
-      const data = await fetchUsers();
-      console.log(data);
-    } catch (error: any) {
-      console.error("Error fetching users:", error.message);
-    }
+    // makeSoapRequest();
   }
 }
 </script>
--- src/services/soapService.ts
+++ src/services/soapService.ts
@@ -1,40 +1,33 @@
 import axios from "axios";
 
-const API_URL = "https://jsonplaceholder.typicode.com/users";
+const SOAP_ENDPOINT = "https://www.mnb.hu/arfolyamok.asmx?wsdl";
+const xmls = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.mnb.hu/webservices/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <web:GetCurrencies/>
+   </soapenv:Body>
+</soapenv:Envelope>`;
 
-// Function to fetch all users using Axios
-export function fetchUsers() {
-  return axios
-    .get(API_URL)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.error("Error fetching users:", error);
-      throw error;
-    });
+export async function makeSoapRequest() {
+  const xmls =
+    '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"\
+                            xmlns:web="http://www.webserviceX.NET/">\
+            <soapenv:Header/>\
+            <soapenv:Body>\
+              <web:ConversionRate>\
+                <web:FromCurrency>INR</web:FromCurrency>\
+                <web:ToCurrency>USD</web:ToCurrency>\
+              </web:ConversionRate>\
+            </soapenv:Body>\
+          </soapenv:Envelope>';
+
+  const res = await axios.post(
+    "http://www.webservicex.com/CurrencyConvertor.asmx?wsdl",
+    xmls,
+    {
+      headers: { "Content-Type": "text/xml" },
+    }
+  );
+
+  console.log(res);
 }
-
-// const SOAP_ENDPOINT = "http://www.mnb.hu/arfolyamok.asmx";
-// const xmls =
-//   '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"\
-//                             xmlns:web="http://www.webserviceX.NET/">\
-//             <soapenv:Header/>\
-//             <soapenv:Body>\
-//               <web:ConversionRate>\
-//                 <web:FromCurrency>INR</web:FromCurrency>\
-//                 <web:ToCurrency>USD</web:ToCurrency>\
-//               </web:ConversionRate>\
-//             </soapenv:Body>\
-//           </soapenv:Envelope>';
-
-// export async function makeSoapRequest() {
-//   axios
-//     .post("http://www.webservicex.com/CurrencyConvertor.asmx?wsdl", xmls, {
-//       headers: { "Content-Type": "text/xml" },
-//     })
-//     .then((res: any) => {
-//       console.log(res);
-//     })
-//     .catch((err: any) => {
-//       console.log(err);
-//     });
-// }
